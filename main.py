300e891db609b162d54d4368da18f709f28ed5c7
Gitea <test@test.test>
2025-06-03 19:05:50 +0200

    statements geordnet, test statements entfernt und clean up

--- main.py
+++ main.py
@@ -4,16 +4,8 @@ from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 from tkinter import ttk
 
-# Hauptfenster erstellen
-
-fenster = tk.Tk()
-fenster.title("Soundboard")
-fenster.geometry("600x300")
-
-# Menü
 
 def datei_oeffnen():
-    mb.showinfo("Öffnen")
     filetypes = (
         ('text files', '*.txt'),
         ('All files', '*.*')
@@ -32,30 +24,29 @@ def datei_oeffnen():
 def datei_speichern():
     mb.showinfo("Speichern")
 
-def datei_beenden():
-    fenster.quit()
 
+# Hauptfenster erstellen
+fenster = tk.Tk()
+fenster.title("Soundboard")
+fenster.geometry("600x300")
+
+# Menü
 button_list = []
 text_variable = ['sound1', 'sound2', 'sound3', 'sound4']
 
-open_button = ttk.Button(
-    fenster,
-    text='Open a File',
-    command=datei_oeffnen)
+open_button = ttk.Button(fenster, text='Open a File', command=datei_oeffnen)
 open_button.pack()
-jingle1 = ttk.Button(fenster, text='Song1🎶', width=20, padding=50)
-play_button = tk.Button(fenster, text="Play")
 
+jingle1 = ttk.Button(fenster, text='Song1🎶', width=20, padding=50)
+jingle1.pack(side='left')
+
+play_button = ttk.Button(fenster, text="Play")
 play_button.pack()
+
 for word in text_variable:
     button2 = ttk.Button(text=word)
-    button2.pack(side='left', expand=True)#wollen wir das nebeneinander haben ?
+    button2.pack(side='left', expand=True)
     button_list.append(button2)
-
-jingle1.pack(side='left')
-end_button = tk.Button(text="Beenden", command=datei_beenden)
-end_button.pack()
-print(jingle1.keys())
 
 
 # GUI starten
